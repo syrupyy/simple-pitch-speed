fd14115f0bd310a151a2c1a1e11f9b1b53d0a01c
syrupyy <77413084+syrupyy@users.noreply.github.com>
2026-03-10 17:52:19 -0400

    feat: add icons and update manifest

--- manifest.config.ts
+++ manifest.config.ts
@@ -3,14 +3,19 @@ import pkg from "./package.json";
 
 export default defineManifest({
   manifest_version: 3,
-  name: pkg.name,
+  name: "Simple Pitch/Speed Changer",
   version: pkg.version,
+  description: "Change the pitch and speed of media elements on web pages.",
   icons: {
-    48: "public/logo.png",
+    16: "public/icon16.png",
+    48: "public/icon48.png",
+    128: "public/icon128.png",
   },
   action: {
     default_icon: {
-      48: "public/logo.png",
+      16: "public/icon16.png",
+      48: "public/icon48.png",
+      128: "public/icon128.png",
     },
     default_popup: "src/popup/index.html",
   },
